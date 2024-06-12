eb14e6e9f85d16190d1d50c87c31befb06ee71a5
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2024-06-12 16:58:12 +0200

    Drafted FAQs section in html, css, js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,29 @@
-// setup nav
+// navbar
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
-// show nav
+
 navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
 });
-// close nav
+
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
+});
+
+// faqs
+
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  btn.addEventListener("click", function () {
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
 });
